f643afd5790d056fba29426d0c4a06eff3a87418
PCBlack3 <pcblack3@gmail.com>
2022-05-14 19:16:49 -0300

    Skeleton created

---
+++ src/main/java/com/myportfolio/pcblack/PcblackApplication.java
@@ -0,0 +1,13 @@
+package com.myportfolio.pcblack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcblackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcblackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myportfolio/pcblack/PcblackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myportfolio.pcblack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcblackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
